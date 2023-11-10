2791e74c781188d5b4aaf4c08ac36a8deb59a352
jsy <1720668440@qq.com>
2023-11-10 09:49:06 +0800

    add gitignore file

--- parent/account-server/src/main/java/com/space/account/infrastructure/constant/GlobalException.java
+++
@@ -1,9 +0,0 @@
-package com.space.account.infrastructure.constant;
-
-import lombok.Data;
-
-@Data
-public class GlobalException extends Exception{
-    private Integer code;
-    private String message;
-}
--- parent/shop-server/src/main/java/com/jsy/shop/interfaces/facade/OrderApi.java
+++
@@ -1,8 +0,0 @@
-package com.jsy.shop.interfaces.facade;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class OrderApi {
-
-}
