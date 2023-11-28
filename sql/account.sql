3aa951d6d3042940185400b1697d0cb6be9d57e7
jsy <1720668440@qq.com>
2023-11-28 18:08:56 +0800

    SQL

--- sql/account.sql
+++ sql/account.sql
@@ -1,50 +1,57 @@
 create database t_account;
+use t_account;
 -- 登录信息表
-drop table if exists t_account.login;
-create table login
+drop table if exists t_account.login_info;
+create table login_info
 (
-    id             bigint primary key not null,
+    id             bigint primary key not null auto_increment,
+    login_account  varchar(32)        not null comment '登陆账号',
     login_name     varchar(32)        not null comment '登录名',
     login_password char(16)           not null comment '登录密码',
     phone          varchar(16) comment '电话号码',
     email          varchar(64) comment '邮箱',
     create_time    datetime,
     update_time    datetime,
-    status         char(1)            not null comment '是否有效'
+    status         char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
 );
--- 用户表
-drop table if exists t_account.user;
-create table t_account.user
+-- 用户信息表
+drop table if exists t_account.user_info;
+create table t_account.user_info
 (
     id                bigint primary key not null,
-    login_id          bigint comment '登录表id',
-    login_name        bigint comment '登录表id',
+    login_account     bigint comment '登录账号',
+    login_name        bigint comment '登录名',
     true_name         varchar(64) comment '真实姓名',
     nick_name         varchar(32) comment '昵称',
-    birthday          datetime comment '生日',
+    birthday          datetime comment '出生年月',
     sex               char(1)            not null comment '性别',
+    id_card           varchar(32) comment '身份证号码',
     country           varchar(64) comment '国籍',
     one_level_address varchar(64) comment '州、省、邦等',
-    city              varchar(32) comment '城市',
+    city              varchar(64) comment '城市',
+    county            varchar(64) comment '县区（限制使用）',
+    certification     char(1)            not null comment '是否实名认证',
     create_time       datetime,
     update_time       datetime,
-    status            char(1)            not null comment '是否有效'
+    status            char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
 );
+
 -- 角色表
-drop table if exists t_account.role;
-create table t_account.role
+drop table if exists t_account.role_info;
+create table t_account.role_info
 (
     id        bigint primary key not null,
     role_name char(16)           not null comment '角色名:administrator',
-    status    char(1)            not null comment '是否有效'
+    status    char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
 );
 -- 用户&角色表
 drop table if exists t_account.user_role;
 create table t_account.user_role
 (
+    id      bigint primary key not null,
     user_id bigint primary key not null,
     role_id bigint             not null,
-    status  char(1)            not null comment '是否有效'
+    status  char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
 );
 -- 权限表
 drop table if exists t_account.role_authority;
@@ -52,8 +59,9 @@ create table t_account.role_authority
 (
     id             bigint primary key not null,
     role_id        bigint             not null,
+    authority_code varchar(16)        not null comment '权限码',
     authority_name char(16)           not null comment '权限名',
-    status         char(1)            not null comment '是否有效'
+    status         char(1)            not null comment '是否有效：0：用户注销，1:使用中，X：违规封禁，Z：系统自动清除'
 );
 -- 收货地址表
 drop table if exists t_account.delivery_address;
@@ -61,26 +69,27 @@ create table delivery_address
 (
     id             bigint primary key not null,
     user_id        bigint             not null comment '用户id',
-    location       varchar(64) comment '区域',
+    location       varchar(64) comment '城市',
     detail_address varchar(64) comment '详细地址',
     receiver       varchar(64) comment '收货人',
     phone          varchar(16) comment '电话号码',
-    tag            char(1) comment '标签',
+    tag            char(1) comment '标签：家：0，公司：1，临时：X',
     default_place  char(1) comment '是否为默认收货地址',
     create_time    datetime,
     update_time    datetime,
-    status         char(1)            not null comment '是否有效'
+    status         char(1)            not null comment '是否有效：0：用户删除，1:使用中'
 );
 -- 优惠券表
 drop table if exists t_account.coupon;
 create table coupon
 (
     id               bigint primary key not null,
-    user_id          bigint             not null comment '持有者id',
+    user_id          bigint             not null comment '用户id',
     discount_type    char(1)            not null comment '打折方式',
-    discount_amount  int comment '打折金额',
-    discount_percent int(3) comment '打折比例',
+    discount_amount  decimal(10, 2) comment '打折金额',
+    discount_percent int(2) comment '打折比例',
     start_price      int                not null comment '满减起始价格',
-    end_date         datetime           not null comment '使用期限',
+    begin_date       datetime           not null comment '开始使用期限',
+    end_date         datetime           not null comment '截止使用期限',
     status           char(1)            not null comment '状态：0：失效，1：有效，2：永久有效'
 );
--- sql/shop.sql
+++ sql/shop.sql
@@ -1,13 +1,16 @@
 create database t_shop;
 -- 订单表
 drop table if exists t_shop.order;
-create table order_info
+create table order_detail
 (
     id          bigint primary key not null,
     serial_num  varchar(64)        not null comment '订单编号',
     user_id     bigint             not null comment '用户id',
+    store_id    bigint             not null comment '店铺id',
     sku_id      bigint             not null comment '商品id',
     address_id  bigint             not null comment '收货地址id',
+    price       decimal(10, 2)     not null comment '价格',
+    pay_price   decimal(10, 2)     not null comment '实付价格',
     create_time datetime,
     update_time datetime,
     status      char(1)            not null comment '是否有效'
@@ -34,13 +37,13 @@ create table store
     status             char(1)            not null comment '是否有效'
 );
 -- 店铺关注表
-drop table if exists t_shop.store;
-create table store
+drop table if exists t_shop.store_follow;
+create table store_follow
 (
     id                bigint primary key not null comment '店铺id',
     collection_amount int(9) comment '收藏量',
-    create_time        datetime,
-    update_time        datetime,
+    create_time       datetime,
+    update_time       datetime,
     status            char(1)            not null comment '是否有效'
 );
 
@@ -59,37 +62,37 @@ create table product
 drop table if exists t_shop.product_sku;
 create table product_sku
 (
-    id         bigint primary key not null,
-    store_id   bigint             not null comment '店铺id',
-    product_id bigint             not null comment '商品id',
-    price      decimal(10, 2)     not null comment '价格',
-    create_time        datetime,
-    update_time        datetime,
-    status     char(1)            not null comment '是否有效'
+    id          bigint primary key not null,
+    store_id    bigint             not null comment '店铺id',
+    product_id  bigint             not null comment '商品id',
+    price       decimal(10, 2)     not null comment '价格',
+    create_time datetime,
+    update_time datetime,
+    status      char(1)            not null comment '是否有效'
 );
 
 -- 商品SKU库存表
 drop table if exists t_shop.product_sku_stork;
 create table product_sku_stork
 (
-    id     bigint primary key not null,
-    sku_id bigint             not null comment 'sku id',
-    stork  bigint             not null comment '库存',
-    create_time        datetime,
-    update_time        datetime,
-    status char(1)            not null comment '是否有效'
+    id          bigint primary key not null,
+    sku_id      bigint             not null comment 'sku id',
+    stork       bigint             not null comment '库存',
+    create_time datetime,
+    update_time datetime,
+    status      char(1)            not null comment '是否有效'
 );
 
 -- 商品详情图片表
-drop table if exists t_shop.product;
-create table product
+drop table if exists t_shop.product_detail_img;
+create table product_detail_img
 (
     id           bigint primary key not null,
     serial_id    int                not null comment '序号',
     product_id   bigint             not null comment '商品id',
     picture_path varchar(32)        not null comment '图片地址',
-    create_time        datetime,
-    update_time        datetime,
+    create_time  datetime,
+    update_time  datetime,
     status       char(1)            not null comment '是否有效'
 );
 -- 商品品类表
@@ -105,18 +108,18 @@ create table product_category
     three_level_category_num  char(3)            not null comment '三级目录编号',
     four_level_category_name  varchar(32)        not null comment '四级目录名(限制使用)',
     four_level_category_num   char(4)            not null comment '四级目录编号(限制使用)',
-    create_time        datetime,
-    update_time        datetime,
+    create_time               datetime,
+    update_time               datetime,
     status                    char(1)            not null comment '是否有效'
 );
 -- 购物车表
 drop table if exists t_shop.shopping_car;
 create table shopping_car
 (
-    id     bigint primary key not null,
-    sku_id bigint             not null comment 'sku id',
-    amount int                not null comment '数量',
-    create_time        datetime,
-    update_time        datetime,
-    status char(1)            not null comment '是否有效'
+    id          bigint primary key not null,
+    sku_id      bigint             not null comment 'sku id',
+    amount      int                not null comment '数量',
+    create_time datetime,
+    update_time datetime,
+    status      char(1)            not null comment '是否有效'
 );
